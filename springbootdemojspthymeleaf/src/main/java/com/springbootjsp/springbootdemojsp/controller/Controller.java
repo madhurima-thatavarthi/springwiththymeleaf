935406c67b752b85454e09aa3c03b9e09f3cc9f3
madhurima-thatavarthi <69160799+madhurima-thatavarthi@users.noreply.github.com>
2021-01-08 15:52:55 +0530

    Add files via upload

---
+++ springbootdemojspthymeleaf/src/main/java/com/springbootjsp/springbootdemojsp/controller/Controller.java
@@ -0,0 +1,25 @@
+package com.springbootjsp.springbootdemojsp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.springbootjsp.springbootdemojsp.model.User;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@org.springframework.stereotype.Controller
+public class Controller {
+
+	@PostMapping(value = "save")
+	public String hello(@ModelAttribute User user) {
+		return "success";
+	}
+	
+	@GetMapping(value = "/one")
+	public String bussinessLogic() {
+		return "HELLO";
+	}
+
+}
